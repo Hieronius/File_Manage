8f680c6947be2755e216e954dc3012d2047a1624
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-24 12:17:22 +0300

    Create a new project

---
+++ File_Manage/File_Manage/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  File_Manage
+//
+//  Created by Арсентий Халимовский on 24.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
