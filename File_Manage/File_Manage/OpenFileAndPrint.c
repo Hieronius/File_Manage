637ea43dcab366368beb571b1d80b016ca96b509
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-06 12:48:58 +0300

    Add method to open and print the content of the file

---
+++ File_Manage/File_Manage/OpenFileAndPrint.c
@@ -0,0 +1,33 @@
+//
+//  OpenFileAndPrint.c
+//  File_Manage
+//
+//  Created by Арсентий Халимовский on 24.12.2023.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main() {
+//    char fileName;
+//    printf("Enter a name of the file to read -> ");
+//    scanf("%s", &fileName);
+//    
+//    FILE *file = NULL;
+//    file = fopen(&fileName, "r");
+//    
+//    if (file == NULL) {
+//        printf("Uncorrent file name");
+//        exit(-1);
+//    }
+//    
+//    char content = fgetc(file);
+//    while (content != EOF) {
+//        printf("%c", content);
+//        content = fgetc(file);
+//    }
+//    
+//    fclose(file);
+//    
+//    return 0;
+//}
