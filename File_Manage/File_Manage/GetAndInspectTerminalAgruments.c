05d352279f95038f7955d6185fe6607088b2d116
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-07 14:47:00 +0300

    Add method to get arguments

---
+++ File_Manage/File_Manage/GetAndInspectTerminalAgruments.c
@@ -0,0 +1,22 @@
+//
+//  GetAndInspectTerminalAgruments.c
+//  File_Manage
+//
+//  Created by Арсентий Халимовский on 26.12.2023.
+//
+
+// MARK: Working with arguments
+
+#include <stdio.h>
+//int main(int argc, char *argv[]) {
+//    printf("\nProgram name: %s", argv[0]);
+//    if (argc < 2) {
+//        printf("No arguments passed through command line!");
+//    } else {
+//        printf("\nArgument supplied: ");
+//        for (int i = 1; i < argc; i++) { // i = 1 because we already have a program name as argv[0] position.
+//            printf("%s\t", argv[i]);
+//        }
+//    }
+//}
+
